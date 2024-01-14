1d9806de35f1c77c7ae38bdef79a3346e83b3fc4
Yevheniia Voitsekhovska <sunakojane6@gmail.com>
2024-01-14 20:59:53 +0200

    homework

---
+++ script/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+let f = 8;
+let m = 3;
+let addition = f + m;
+console.log(addition);
+
+let subtraction = f - m;
+console.log(subtraction);
+
+let multiplication = f * m;
+console.log(multiplication);
+
+let division = f / m;
+console.log(division);
+
+console.log(f ** 2);
